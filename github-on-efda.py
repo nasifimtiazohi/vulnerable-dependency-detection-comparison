77aa2ecfd9bc416f5e8df4ebd2815fd74e5b5d39
nasifimtiazohi <nasifimtiaz88@gmail.com>
2019-12-16 13:06:58 -0500

    test commit to see how github security alertsact

---
+++ github-on-efda.py
@@ -0,0 +1,9 @@
+'''measuring performance of github's dependecy graph and security alerts tool'''
+
+'''
+1) clone a github repo
+2) enable dependency-graph and security alerts
+3) api call to read and parse the dependency and alerts
+'''
+
+print('hello world')
