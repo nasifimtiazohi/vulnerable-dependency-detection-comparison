7220a2b9363caf3e0e74a1eddde58a4423eb2c42
nasifimtiazohi <nasifimtiaz88@gmail.com>
2020-05-14 19:10:05 -0400

    completed graphql api query to get dependency alerts of a repo

--- gh_graphql.py
+++ gh_graphql.py
@@ -1,13 +1,13 @@
 import os
 import requests
 
-headers = {"Authorization": "token {}".format(os.environ['github_token'])}
+headers = {"Authorization": "token {}".format(os.environ['token'])}
 
-def run_query(query): 
+def run_query(query, variables): 
 		request = requests.post('https://api.github.com/graphql', 
-        json={'query': query}, headers=headers)
+        json={'query': query, 'variables':variables}, headers=headers)
 		if request.status_code == 200:
-			return request.json()
+			return request.json()['data']
 		else:
 			raise Exception("Query failed to run by returning code of {}. {}".format(
                     request.status_code, query))
@@ -15,16 +15,55 @@ def run_query(query):
 
 def getDependencyAlerts(repo_owner, repo_name):
     query=''' 
-        {
-            repository(owner: "nasifimtiazohi" , name: "openmrs-core" ) {
-                vulnerabilityAlerts(first: 10) {
-                    nodes {
-                        id
+            query($repo_owner: String!, $repo_name: String!, $after: String )  { 
+                repository(owner: $repo_owner , name: $repo_name ) {
+                    vulnerabilityAlerts(last:100, after: $after ) {
+                        totalCount
+                        nodes {
+                            id
+                            createdAt
+                            securityAdvisory{
+                            description
+                            origin
+                            identifiers{
+                                type
+                                value
+                            }
+                            }
+                            securityVulnerability{
+                            package{
+                                name
+                            }
+                            }
+                        }
+                        pageInfo{
+                            hasNextPage
+                            endCursor
+                        }
                     }
                 }
-            }
-        }
+    }
     '''
-    return run_query(query)
+    variables={
+            "repo_owner": "nasifimtiazohi",
+            "repo_name": "openmrs-core",
+            "after": None
+            }
+
+    totalCount=None
+    alerts=[]
+    while True:
+        data=  run_query(query, variables)['repository']['vulnerabilityAlerts']
+        totalCount=data['totalCount']
+        alerts.extend(data['nodes'])
+        if data['pageInfo']['hasNextPage']:
+            variables["after"]=data['pageInfo']['endCursor']
+        else:
+            break
+        
+    if len(alerts)==totalCount:
+        return alerts
+    else:
+        raise Exception('graphql call not functioning properly,')
 if __name__=='__main__':
     print(getDependencyAlerts('nasifimtiazohi','openmrs-core'))
