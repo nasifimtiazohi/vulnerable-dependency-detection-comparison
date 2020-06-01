7f22ba1fc4d90a0e7ba85b1c2b2509202ed1e7f8
nasifimtiazohi <nasifimtiaz88@gmail.com>
2020-06-01 18:40:25 -0400

    refactored owasp scanning

--- analyzeOWASP.py
+++ analyzeOWASP.py
@@ -1,5 +1,5 @@
 import sys, os
-import sql
+import common, sql
 import csv
 import pandas as pd
 
@@ -10,31 +10,31 @@ def getModuleId(module):
     return results[0]['id']
 
 def redesignColumns(df):
-    keep=['idmodule','ScanDate', 'DependencyName', 'Description',
+    keep=['ScanDate', 'DependencyName', 'Description',
         'Identifiers','CPE', 'CVE',
        'CWE', 'Vulnerability', 'Source', 'CVSSv2_Severity', 'CVSSv2_Score',
        'CVSSv3_BaseSeverity', 'CVSSv3_BaseScore', 'CPE Confidence' ]
     df=df[keep]
-    new_names=['idmodule','scandate', 'dependency', 'description',
+    new_names=['scandate', 'dependency', 'description',
         'package','CPE' , 'CVE',
        'CWE', 'vulnerability', 'source', 'CVSS2_severity', 'CVSS2_score',
        'CVSS3_severity', 'CVSS3_score', 'confidence' ]
     df.columns= new_names
     return df
 
-def addPackage(group,artifact,version):
-    q="select id from packages where `group`='{}' and artifact='{}' and version ='{}'".format(group,artifact,version)
-    results=sql.execute(q)
-    if not results:
-        sql.execute("insert into packages values (null,'{}','{}','{}');".format(group,artifact,version))
-        results=sql.execute(q)
-    return sql.execute(q)[0]['id']
+# def addPackage(group,artifact,version):
+#     q="select id from packages where `group`='{}' and artifact='{}' and version ='{}'".format(group,artifact,version)
+#     results=sql.execute(q)
+#     if not results:
+#         sql.execute("insert into packages values (null,'{}','{}','{}');".format(group,artifact,version))
+#         results=sql.execute(q)
+#     return sql.execute(q)[0]['id']
 def getPackageId(dependency, identifier):
     dep=str(dependency)
     if dep.endswith('.jar'):
         dep=dep[:-4]
         q='''select id
-            from packages
+            from package
             where concat(artifact,'-',version) = '{}';'''.format(dep)
         return sql.execute(q)[0]['id']
     else:
@@ -48,15 +48,16 @@ def getPackageId(dependency, identifier):
         temp=temp.split("@")
         artifact=temp[0]
         version=temp[1]
-        return addPackage(group,artifact,version)
+        source='owasp'
+        return common.getPackageId(group,artifact,version,source)
     
-def getDependencyId(idmodule, idpackage):
-    selectQ='''select id from dependencyTree where 
-            idmodule={} and idpackage={}'''.format(idmodule,idpackage)
+def getDependencyId(repoId, packageId):
+    selectQ='''select id from dependency where 
+            repositoryId={} and packageId={}'''.format(repoId,packageId)
     results = sql.execute(selectQ)
     if not results:
-        insertQ='''insert into dependencyTree values (null,
-                    {},{},'external',null,null); '''.format(idmodule,idpackage)
+        insertQ='''insert into dependency values (null,
+                    {},{}); '''.format(repoId,packageId)
         sql.execute(insertQ)
         results = sql.execute(selectQ)
     return results[0]['id']
@@ -66,7 +67,7 @@ def getDependencyId(idmodule, idpackage):
 
 
 
-def getVulnerabiltyId(idpackage, source, cve, cwe, cpe,
+def getVulnerabiltyId(packageId, source, cve, cwe, cpe,
                     description, vulnerability, CVSS2_severity,
                     CVSS2_score, CVSS3_severity, CVSS3_score ):
     if cve.startswith('CVE'):
@@ -83,14 +84,14 @@ def getVulnerabiltyId(idpackage, source, cve, cwe, cpe,
     vulnerability=vulnerability.replace('"','\\"')
 
 
-    selectQ= '''select id from vulnerabilities
-        where idpackage="{}" and CVE ="{}" and nonCVE="{}"'''.format(idpackage,CVE,nonCVE)
+    selectQ= '''select id from vulnerability
+        where packageId="{}" and CVE ="{}" and nonCVE="{}"'''.format(packageId,CVE,nonCVE)
     results=sql.execute(selectQ)
     if results:
         return results[0]['id']
     else:
-        insertQ='''insert into vulnerabilities values (null,
-        {},"{}","{}","{}","{}","{}","{}","{}","{}",{},"{}",{});'''.format(idpackage,
+        insertQ='''insert into vulnerability values (null,
+        {},"{}","{}","{}","{}","{}","{}","{}","{}",{},"{}",{});'''.format(packageId,
         source,CVE,nonCVE,cwe,cpe,description,vulnerability,CVSS2_severity,CVSS2_score,
         CVSS3_severity,CVSS3_score)
         try:
@@ -103,34 +104,46 @@ def getVulnerabiltyId(idpackage, source, cve, cwe, cpe,
 
 
 #path to openmrs
-path= "/Users/nasifimtiaz/openmrs"
-os.chdir(path)
-depfilename="dependency-check-report.csv"
-files=(os.popen('find ./ -name "{}"'.format(depfilename)).read()).split("\n")[:-1]
-for file in files:
-    df= pd.read_csv(file, sep=',')
-    
-    if len(df) == 0:
-        continue
+def process_alerts(path):
+    os.chdir(path)
+    repo=path.split('/')[-1]
+    repoId=common.getRepoId(repo)
+    depfilename="dependency-check-report.csv"
+    #files=(os.popen('find ./ -name "{}"'.format(depfilename)).read()).split("\n")[:-1]
+    files=['./target/'+depfilename] #will only read the root file
 
-    idmodule=getModuleId(df.iloc[0,0])
-    df.insert(loc=0,column='idmodule',value=[idmodule]*len(df))
-    df=redesignColumns(df)
+    for file in files:
+        df= pd.read_csv(file, sep=',')
+        
+        if len(df) == 0:
+            continue
+        
+        df=redesignColumns(df)
 
-    df['idpackage']=df.apply(lambda row: getPackageId(row.dependency, row.package),axis=1)
-    
-    df = df.astype(object).where(pd.notnull(df),'null')
+        df['repoId']=[repoId]*len(df)
 
-    df['idvulnerability']=df.apply(lambda row: getVulnerabiltyId(row.idpackage, row.source, 
-                    row.CVE, row.CWE, row.CPE,
-                    row.description, row.vulnerability, row.CVSS2_severity,
-                    row.CVSS2_score, row.CVSS3_severity, row.CVSS3_score ), axis=1)
+        df['packageId']=df.apply(lambda row: getPackageId(row.dependency, row.package),axis=1)
+        
+        df = df.astype(object).where(pd.notnull(df),'null')
 
-    df['iddependency']=df.apply(lambda row: getDependencyId(row.idmodule, row.idpackage), axis=1) 
-    df['tool']='owasp'
-    df=df[['scandate','iddependency','idvulnerability','confidence','tool']]
+        df['vulnerabilityId']=df.apply(lambda row: getVulnerabiltyId(row.packageId, row.source, 
+                        row.CVE, row.CWE, row.CPE,
+                        row.description, row.vulnerability, row.CVSS2_severity,
+                        row.CVSS2_score, row.CVSS3_severity, row.CVSS3_score ), axis=1)
 
-    
-    #sql.load_df('alerts',df)
+        df['dependencyId']=df.apply(lambda row: getDependencyId(row.repoId, row.packageId), axis=1) 
+        df['tool']='owasp'
+        df=df[['scandate','dependencyId','vulnerabilityId','confidence','tool']]
+
+        
+        sql.load_df('alert',df)
+
+
+if __name__=='__main__':
+    repos=common.getWatchedRepos()
+    for path in repos:
+        os.chdir(path)
+        #os.system('mvn org.owasp:dependency-check-maven:aggregate -Dformat=CSV')
+        process_alerts(path)
     
     
