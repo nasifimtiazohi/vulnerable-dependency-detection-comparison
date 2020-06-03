f5bceb82cca3c026fe76fc441a2a2a7fc34adb8e
nasifimtiazohi <nasifimtiaz88@gmail.com>
2020-06-03 00:51:31 -0400

    refactored snyk code

--- analyzeSnyk.py
+++ analyzeSnyk.py
@@ -1,46 +1,36 @@
 import os, json
 import common, sql
 
-def getDependencyId(repo, group,artifact):
-    query='''select dT.id
-                from modules m
-                join dependencyTree dT
-                on m.id=dT.idmodule
-                join packages p on
-                    p.id = dT.idpackage
-                where m.repository='{}'
-                and p.`group`='{}'
-                and p.artifact='{}';'''.format(repo, group, artifact)
-    return sql.execute(query)[0]['id']
-
 if __name__=='__main__':
     repos= common.getWatchedRepos()
 
     for path in repos:
         os.chdir(path)
         repo= path.split('/')[-1]
+        repoId=common.getRepoId(repo)
         report= json.loads(os.popen('snyk test --json').read())
         for vuln in report['vulnerabilities']:
             group=vuln['mavenModuleName']['groupId']
             artifact=vuln['mavenModuleName']['artifactId']
             version=vuln['version']
-            iddependency= getDependencyId(repo,group,artifact)
-            idpackage=common.getPackageId(group,artifact,version)
+
+            packageId = common.getPackageId(group,artifact,version)
+            dependencyId = common.getDependencyId(repoId, packageId)
+
             cves=vuln['identifiers']['CVE']
             if cves:
-                
                 for cve in cves: 
-                    q="select id from vulnerabilities where CVE='{}'".format(cve)
+                    q="select id from vulnerability where CVE='{}'".format(cve)
                     results=sql.execute(q)
                     if not results:
-                        common.addFromNvdApi(cve,idpackage)
-                        q="select id from vulnerabilities where CVE='{}'".format(cve)
+                        common.addFromNvdApi(cve,packageId)
+                        q="select id from vulnerability where CVE='{}'".format(cve)
                         results=sql.execute(q)
                     
-                    idvulnerability=results[0]['id']
+                    vulnerabilityId=results[0]['id']
 
-                    q="insert into alerts values(null,{},{},null,'snyk');".format(
-                        str(iddependency), str(idvulnerability))  
+                    q="insert into alert values(null,{},{},null,'snyk');".format(
+                        str(dependencyId), str(vulnerabilityId))  
                     sql.execute(q)
             else:
                 #process new vulnerabilty 
@@ -50,19 +40,19 @@ if __name__=='__main__':
                 vulnerability=vuln['title']
                 CVSS3severity=vuln['severity']
                 cwe=','.join(vuln['identifiers']['CWE'])
-                selectQ='select id from vulnerabilities where nonCVE="{}";'.format(noncve)
+                selectQ='select id from vulnerability where nonCVE="{}";'.format(noncve)
                 results=sql.execute(selectQ)
                 if not results:
-                    q='''insert into vulnerabilities values(null,
+                    q='''insert into vulnerability values(null,
                         {},'snyk',null, '{}', '{}',null, '{}','{}',null,null,'{}',null
-                    );'''.format(idpackage,noncve,cwe,description,vulnerability,CVSS3severity)
+                    );'''.format(packageId,noncve,cwe,description,vulnerability,CVSS3severity)
                     sql.execute(q)
                     results=sql.execute(selectQ)
 
-                idvulnerability=results[0]['id']
+                vulnerabilityId=results[0]['id']
 
-                q="insert into alerts values(null,{},{},null,'snyk');".format(
-                        str(iddependency), str(idvulnerability))  
+                q="insert into alert values(null,{},{},null,'snyk');".format(
+                        str(dependencyId), str(vulnerabilityId))  
                 sql.execute(q)
 
         
