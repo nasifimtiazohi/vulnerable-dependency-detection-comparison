e5b3a82adaae62a2c27ea3974323b3d605549738
nasifimtiazohi <nasifimtiaz88@gmail.com>
2020-09-27 18:11:07 -0400

    completed steady

--- tool_scanning/analyzeSteady.py
+++ tool_scanning/analyzeSteady.py
@@ -2,9 +2,11 @@ import os, sys
 sys.path.append('..')
 import sql, common
 import json
+import distro_information.prepareDistro as distro
+from dateutil import parser as dt
+from datetime import datetime 
 
 toolId= common.getToolId('Steady')
-
 
 hm={
     'vulnerableVersion' : 1,
@@ -14,13 +16,7 @@ hm={
     'nonVulnerableLibraryCode':5,
     'vulnerableLibraryCode':6            
     }
-
 inv_hm = {v:k for k, v in hm.items()}
-
-def getRepositoryId(data):
-    group, artifact, version = data['groupId'], data['artifactId'], data['version']
-    q='select id from repository where `group`=%s and artifact=%s and version=%s'
-    return sql.execute(q,(group, artifact, version))[0]['id']
 
 def addSteadyVulnerability(vuln):
     #process this vuln
@@ -37,6 +33,7 @@ def addSteadyVulnerability(vuln):
     return common.getVulnerabilityId(None,sourceId)
     
 def getAlertId(scandate, dependencyId, vulnerabilityId):
+    print(scandate, dependencyId, vulnerabilityId)
     insertQ = 'insert into mavenAlert values(%s,%s,%s,%s,%s,%s,%s,%s)'
     try:
         sql.execute(insertQ,(None,scandate,dependencyId,vulnerabilityId,
@@ -53,98 +50,128 @@ def getAlertId(scandate, dependencyId, vulnerabilityId):
     return sql.execute(selectQ,(dependencyId,vulnerabilityId,toolId))[0]['id']
     
 
-def processReport(file):
-     #read the root level target directory
-    data= json.loads(open(file,'r').read())['vulasReport']
-    
-    repoId = getRepositoryId(data['geenratedFor'])
-
-    scandate= data['generatedAt']
-    
-    vulnerabilities=data['vulnerabilities']
-
-    for vuln in vulnerabilities:
+def processReport(repoName, repoId):
+    filename = repoName + '-vulas.json'
+    with open(filename,'r') as file:
+        data= json.loads(file.read())['vulasReport']
         
-        #get package id
-        package=vuln['filename']
-        if not package.endswith('.jar') or 'jar' in package[:-4]:
-            #second condition ensures single package
-            raise Exception('package not jar',package)
+        if 'vulnerabilities' not in data.keys() or len(data['vulnerabilities'])==0:
+            return
 
-        package=package[:-4]
-        q='''select id
-            from package
-            where concat(artifact,'-',version,'.jar') = %s;'''
-        packageId=sql.execute(q,(package,))[0]['id']
-        dependencyId=common.getDependencyId(repoId,packageId)
+        scandate= data['generatedAt']
+        
+        vulnerabilities = data['vulnerabilities']
+
+        for vuln in vulnerabilities:
             
-        #get cve id
-        if vuln['bug']['id'].startswith('CVE') and not 'CVE' in vuln['bug']['id'][3:]:
-            #second condition ensures single CVE
-            cve= vuln['bug']['id']
-            if len(cve.split('-'))>3:
-                #some in vulas has extra addendums
-                print("check",cve)
-                cve='-'.join(x for x in cve.split('-')[:3])
+            #get package id
+            package=vuln['filename']
+            if not package.endswith('.jar') or 'jar' in package[:-4]:
+                #second condition ensures single package
+                raise Exception('package not jar',package)
+
+            package=package[:-4]
+            q='''select id
+                from package
+                where concat(artifact,'-',version) = %s;'''
+            packageId=sql.execute(q,(package,))[0]['id']
+            dependencyId=common.getDependencyId(repoId,packageId)
+                
             
-            vulnerabilityId=common.getVulnerabilityId(cve,None)
+            steadyId = True
+            #get cve id
+            if vuln['bug']['id'].startswith('CVE') and not 'CVE' in vuln['bug']['id'][3:]:
+                #second condition ensures single CVE
+                cve= vuln['bug']['id']
+                if len(cve.split('-'))>3:
+                    #some in vulas has extra addendums
+                    print("check",cve)
+                    cve='-'.join(x for x in cve.split('-')[:3])
+                
+                vulnerabilityId=common.getVulnerabilityId(cve,None)
+                if vulnerabilityId > 0:
+                    steadyId = False
+                
+            if steadyId:
+                vulnerabilityId=addSteadyVulnerability(vuln)
+                
+
             
+            integrationTest=[]
+            #DONE: write in a way so that first it can be run for only unit test
+            #then again for integration test as well
+            #check if already alert is pushed for the first three
+            
+            alertId = getAlertId(scandate,dependencyId,vulnerabilityId)
+            
+            vulnerableVersion=[]
+            staticAnalysis=[]
+            unitTest=[]
+            
+            for module in vuln['modules']:
+                #get the static and dynamic analysis results into a separate table
+                vulnerableVersion.append(hm[module['containsVulnerableCode']])
+                staticAnalysis.append(hm[module['potentiallyExecutesVulnerableCode']])
+                unitTest.append(hm[module['actuallyExecutesVulnerableCode']])
+            
+            vv=max(vulnerableVersion)
+            sa=max(staticAnalysis)
+            ut=max(unitTest)
+
+            q='insert into steady values(%s,%s,%s,%s,%s)'
+            try:
+                sql.execute(q,(alertId,inv_hm[vv],inv_hm[sa],inv_hm[ut],None))
+            except sql.pymysql.IntegrityError as error:
+                if error.args[0] == sql.PYMYSQL_DUPLICATE_ERROR:
+                    #TODO update scandate
+                    print('maven alert exists already in steady db')     
+                else:  
+                    raise Exception(str(error))
+                
+            #after running integration test ut will change
+            # alertId=results[0]['id']
+            # integrationTest=[]
+            # for module in vuln['modules']:
+            #     integrationTest.append(hm[module['actuallyExecutesVulnerableCode']])
+            # it=max(integrationTest)
+            # q='''update alert set integrationTest='{}'
+            #         where alertId={} '''.format(inv_hm[it],alertId)
+            # print(q)
+    
+    
+
+
+def getScanTime(repoName):
+    print(repoName)
+    filename= repoName+'-log.txt'
+    with open (filename, 'r') as file:
+        lines= file.readlines()
+        if lines:
+            time = lines[-1]
         else:
-            vulnerabilityId=addSteadyVulnerability(vuln)
-            
-
-        
-        integrationTest=[]
-        #DONE: write in a way so that first it can be run for only unit test
-        #then again for integration test as well
-        #check if already alert is pushed for the first three
-        
-        alertId = getAlertId(scandate,dependencyId,vulnerabilityId)
-        
-        vulnerableVersion=[]
-        staticAnalysis=[]
-        unitTest=[]
-        
-        for module in vuln['modules']:
-            #get the static and dynamic analysis results into a separate table
-            vulnerableVersion.append(hm[module['containsVulnerableCode']])
-            staticAnalysis.append(hm[module['potentiallyExecutesVulnerableCode']])
-            unitTest.append(hm[module['actuallyExecutesVulnerableCode']])
-        
-        vv=max(vulnerableVersion)
-        sa=max(staticAnalysis)
-        ut=max(unitTest)
-
-        #TODO: get alert ID
-        q='insert into steady values(%s,%s,%s,%s,%s)'
-        try:
-            sql.execute(q,(alertId,inv_hm[vv],inv_hm[sa],inv_hm[ut],None))
-        except sql.pymysql.IntegrityError as error:
-            if error.args[0] == sql.PYMYSQL_DUPLICATE_ERROR:
-                #TODO update scandate
-                print('maven alert exists already in steady db')     
-            else:  
-                raise Exception(str(error))
-            
-        #after running integration test ut will change
-        # alertId=results[0]['id']
-        # integrationTest=[]
-        # for module in vuln['modules']:
-        #     integrationTest.append(hm[module['actuallyExecutesVulnerableCode']])
-        # it=max(integrationTest)
-        # q='''update alert set integrationTest='{}'
-        #         where alertId={} '''.format(inv_hm[it],alertId)
-        # print(q)
+            time='00:05:00.000000'
     
+    hour, min, sec = time.split(':')
+    return int(hour)*60 + int(min)
     
+        
+
 
 if __name__=='__main__':
-    #Note: applications should already be scanned in the vulas VM and
-    # generated reports should be synced to the machine where this code is running
-    repos= common.getWatchedRepos()
-    for path in repos:
-        os.chdir(path)
-        repo=path.split('/')[-1]
-        repoId=common.getRepoId(repo)
-
+    repoRelaseMapping = distro.getRepoReleaseMapping()
+    reportPath = '/Users/nasifimtiaz/Desktop/runSteady/reports'
+    os.chdir(reportPath)
+    time = 0
+    for repo in repoRelaseMapping.keys():
+        if repo == 'openmrs-owa-sysadmin':
+            continue
+        repoName = repo + '-' + repoRelaseMapping[repo]
+        repoId = common.getRepoId(repo)
+        time += getScanTime(repoName)
+        processReport(repoName, repoId)
+    
+    common.addScanTime(toolId, time, 'maven')
+        
+    
+        
        
